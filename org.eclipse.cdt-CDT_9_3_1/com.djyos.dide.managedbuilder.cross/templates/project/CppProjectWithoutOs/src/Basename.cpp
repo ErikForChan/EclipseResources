2bd3d7e64003fd64c48f126651251c8d24541365
guoxp <guoxp@98a3a3af-8837-1c49-9e9e-13921c22655a>
2018-09-30 06:43:38 +0000

    git-svn-id: https://xiangmuzuserver/svn/%E7%A1%AC%E4%BB%B6%E7%BB%84%E5%BC%80%E5%8F%91%E5%BA%93/platform_soft/djyos/DIDE/EclipsePluginSource@44092 98a3a3af-8837-1c49-9e9e-13921c22655a

---
+++ org.eclipse.cdt-CDT_9_3_1/com.djyos.dide.managedbuilder.cross/templates/project/CppProjectWithoutOs/src/Basename.cpp
@@ -0,0 +1,12 @@
+/*
+ * Name        : $(baseName).c
+ * Author      : $(author)
+ * Version     :
+ * Copyright   : $(copyright)
+ * Description : main function in C++
+ */
+
+extern "C" int main() {
+	/* TODO */
+	return 0;
+}
