17ad617ab478feb5b48e19c8c24f4228d0520da0
guoxp <guoxp@98a3a3af-8837-1c49-9e9e-13921c22655a>
2018-09-30 04:02:30 +0000

    git-svn-id: https://xiangmuzuserver/svn/%E7%A1%AC%E4%BB%B6%E7%BB%84%E5%BC%80%E5%8F%91%E5%BA%93/platform_soft/djyos/DIDE/EclipsePluginSource@44087 98a3a3af-8837-1c49-9e9e-13921c22655a

---
+++ org.eclipse.jface/com.djyos.dide.managedbuilder.cross/templates/project/Flash/src/driver.c
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Hangzhou C-SKY Microsystems Co., Ltd.All rights reserved.
+ *******************************************************************************/
+
+/**
+ * Driver for flash program.
+ */
+
+/**
+ * ERROR TYPE. MUST NOT BE MODIFIED
+ */
+#define ERROR_INIT      -200
+#define ERROR_READID    -201
+#define ERROR_PROGRAM   -202
+#define ERROR_READ      -203
+#define ERROR_ERASE     -204
+#define ERROR_CHIPERASE -205
+#define ERROR_UNINIT	-206
+
+/**
+ * Customize this method to perform any initialization
+ * needed to access your flash device.
+ *
+ * @return: if this method returns an error,MUST RUTURN ERROR_INIT,
+ * Otherwise return 0.
+ */
+int  flashInit(){
+	/* TODO */
+  return 0;
+}
+
+/**
+ * Customize this method to perform any un-initialization
+ * needed to access your flash device.
+ *
+ * @return: if this method returns an error,MUST RUTURN ERROR_UNINIT,
+ * Otherwise return 0.
+ */
+int  flashUnInit(){
+	/* TODO */
+  return 0;
+}
+
+/**
+ * Customize this method to read flash ID
+ *
+ * @param flashID: returns for flash ID
+ *
+ * @return: if this method returns an error,MUST RUTURN ERROR_READID,
+ * Otherwise return 0.
+ */
+int  flashID(unsigned int* flashID){
+	/* TODO */
+  return 0;
+}
+
+/**
+ * This method takes the data pointed to by the src parameter
+ * and writes it to the flash blocks indicated by the
+ * dst parameter.
+ *
+ * @param dst : destination address where flash program
+ * @param src : address of BUFF in RAM, take the data of this address write to the flash
+ * @param length : data length
+ *
+ * @return : if this method returns an error,MUST RUTURN ERROR_PROGRAM,
+ * Otherwise return 0.
+ */
+int flashProgram(char* dst, char *src, int length){
+	/* TODO */
+  return 0;
+}
+
+/**
+ * Customize this method to read data from a group of flash blocks into a buffer
+ *
+ * @param dst : address of BUFF in RAM, read flash date into this address 
+ * @param src : destination address where flash read
+ * @param length : data length
+ *
+ *  @return: if this method returns an error,MUST RUTURN ERROR_READ,
+ * Otherwise return 0.
+ */
+int flashRead(char* dst, char *src, int length){
+	/* TODO */
+  return 0;
+}
+
+/**
+ * Customize this method to erase a group of flash blocks.
+ *
+ * @param dst : a pointer to the base of the flash device.
+ * @param length : erase length
+ *
+ * @return : if this method returns an error,MUST RUTURN ERROR_ERASE,
+ * Otherwise return 0
+ */
+int flashErase(char *dst, int length){
+	/* TODO */
+  return 0;
+}
+
+/**
+ * Customize this method to erase the whole flash.
+ *
+ * @return : if this method returns an error,MUST RUTURN ERROR_CHIPERASE,
+ * Otherwise return 0.
+ */
+int flashChipErase(){
+	/* TODO */
+  return 0;
+}
+
+/* 
+ * NOTING: when debug the driver, this macro defined as 1, and then
+ * it must be set as 0, for release to flash programmer library
+ */
+#define DEBUG_DRIVER	0
+
+/**
+ * Debug entry for driver.
+ *
+ * @return : if this method returns an error,MUST RUTURN ERROR_CHIPERASE,
+ * Otherwise return 0.
+ */
+int flashTest(){
+#if DEBUG_DRIVER
+
+	unsigned int ID;
+	/* read flash id */
+	flashID(&ID);
+
+	/* other drivers test */
+#else
+	return 0;
+#endif
+}
+
---
+++ org.eclipse.jface/com.djyos.dide.managedbuilder.cross/templates/project/helloWorldCppProject/src/Basename.cpp
@@ -0,0 +1,13 @@
+/*
+ * Name        : $(baseName).c
+ * Author      : $(author)
+ * Version     :
+ * Copyright   : $(copyright)
+ * Description : Hello World in C, Ansi-style
+ */
+#include <stdio.h>
+
+int main() {
+	printf ("$(message)\n"); /* prints "Hello World!" */
+	return 0;
+}
