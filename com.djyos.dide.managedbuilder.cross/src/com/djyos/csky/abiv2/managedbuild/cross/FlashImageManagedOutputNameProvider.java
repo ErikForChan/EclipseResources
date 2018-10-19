6ec729cd5e250087a6428eafff3a40a82306e96b
guoxp <guoxp@98a3a3af-8837-1c49-9e9e-13921c22655a>
2018-10-19 07:30:37 +0000

    修改CSky工具链,与ARM工具链保持一致。
    git-svn-id: https://xiangmuzuserver/svn/%E7%A1%AC%E4%BB%B6%E7%BB%84%E5%BC%80%E5%8F%91%E5%BA%93/platform_soft/djyos/DIDE/EclipsePluginSource@44420 98a3a3af-8837-1c49-9e9e-13921c22655a

---
+++ com.djyos.dide.managedbuilder.cross/src/com/djyos/csky/abiv2/managedbuild/cross/FlashImageManagedOutputNameProvider.java
@@ -0,0 +1,41 @@
+package com.djyos.csky.abiv2.managedbuild.cross;
+
+import org.eclipse.cdt.managedbuilder.core.IManagedOutputNameProvider;
+import org.eclipse.cdt.managedbuilder.core.IOption;
+import org.eclipse.cdt.managedbuilder.core.ITool;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+public class FlashImageManagedOutputNameProvider implements IManagedOutputNameProvider {
+
+	// ------------------------------------------------------------------------
+
+	public IPath[] getOutputNames(ITool tool, IPath[] primaryInputNames) {
+
+		String value = null;
+
+		IOption option = tool.getOptionBySuperClassId(Option.OPTION_CREATEFLASH_CHOICE);
+		if (option != null)
+			value = (String) option.getValue();
+		// System.out.println(value);
+
+		String ext = "unknown";
+		if (value != null) {
+			if (value.endsWith("." + Option.CHOICE_IHEX))
+				ext = "hex";
+			else if (value.endsWith("." + Option.CHOICE_SREC))
+				ext = "srec";
+			else if (value.endsWith("." + Option.CHOICE_SYMBOLSREC))
+				ext = "symbolsrec";
+			else if (value.endsWith("." + Option.CHOICE_BINARY))
+				ext = "bin";
+		}
+
+		IPath[] iPath = new IPath[1];
+		iPath[0] = new Path("${BuildArtifactFileBaseName}." + ext);
+		return iPath;
+	}
+
+	// ------------------------------------------------------------------------
+}
+
